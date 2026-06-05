b15b8694f03e736dc613df565e5dccf706dba26a
szubixyt-dev <szubixyt@gmail.com>
2026-06-05 15:14:36 +0200

    Update Instalator Apki.bat

--- Instalator Apki.bat
+++ Instalator Apki.bat
@@ -77,7 +77,7 @@ mkdir "%APPDIR%" 2>nul
 echo Pobieranie plików...
 
 :: pobieranie
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/szubixyt-dev/Apki/main/Apkiwin10.zip' -OutFile '%TEMP%\apki.zip'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/szubixyt-dev/Apki/main/Apki.zip' -OutFile '%TEMP%\apki.zip'"
 
 echo Wypakowywanie...
 
@@ -127,7 +127,7 @@ mkdir "%APPDIR%" 2>nul
 echo Pobieranie plików...
 
 :: pobieranie
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/szubixyt-dev/Apki/main/Apki.zip' -OutFile '%TEMP%\apki.zip'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/szubixyt-dev/Apki/main/Apkiwin10.zip' -OutFile '%TEMP%\apki.zip'"
 
 echo Wypakowywanie...
 
@@ -166,7 +166,7 @@ mkdir "%APPDIR%" 2>nul
 echo Pobieranie plików...
 
 :: pobieranie
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/szubixyt-dev/Apki/main/Apki.zip' -OutFile '%TEMP%\apki.zip'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/szubixyt-dev/Apki/main/Apkiwin10.zip' -OutFile '%TEMP%\apki.zip'"
 
 echo Wypakowywanie...
 
